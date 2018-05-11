ec76c505a06d7d0e2e85b43e0e04d4f2579a5717
domaincrisman <cmanzanore@gmail.com>
2018-05-11 21:40:31 +0200

    Update Client and Employee Index

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -282,7 +282,7 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
+  # config.footer = 'Powered by Active Admin for Remote Devs 1.0'
 
   # == Sorting
   #
